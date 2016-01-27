53507dc7fe3ce2682dee08de13eb09bf7f783f99
Allan Stewart <stewart.allan@gmail.com>
2016-01-26 17:07:23 -0700

    Log routes on startup for reference

--- server.js
+++ server.js
@@ -10,4 +10,17 @@ apiRoutes.registerRoutes(app, {serverVersion: serverVersion});
 
 app.listen(3000, function () {
   console.log('OpenStarDrive server (v' + serverVersion + ') listening on port 3000!');
+  logRoutes();
 });
+
+function logRoutes() {
+  console.log('Routes:')
+  app._router.stack.forEach(function (item) {
+    if (item.route && item.route.path) {
+      var methods = Object.keys(item.route.methods);
+      methods.forEach(function (method) {
+        console.log(method.toUpperCase(), item.route.path);
+      });
+    }
+  });
+}
