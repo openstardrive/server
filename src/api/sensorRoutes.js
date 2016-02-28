7361153a55a698fb6fdf294227e9e74b19e7d2cc
Allan Stewart <stewart.allan@gmail.com>
2016-02-27 21:01:49 -0700

    Add sensor contact update and remove routes

--- src/api/sensorRoutes.js
+++ src/api/sensorRoutes.js
@@ -6,4 +6,17 @@ exports.registerRoutes = function (app, config) {
     var contact = sensors.addContact(request.body);
     response.send(contact);
   });
+
+  app.put('/api/systems/sensors/contacts/:id', function (request, response) {
+    if (request.params.id != request.body.id) {
+      return response.status(400).send({error: 'Contact id mismatch.'});
+    }
+    var contact = sensors.updateContact(request.body);
+    response.send(contact);
+  });
+
+  app.delete('/api/systems/sensors/contacts/:id', function (request, response) {
+    var contact = sensors.removeContact(request.params.id);
+    response.send(contact);
+  });
 };
