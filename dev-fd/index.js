322d35b0a1f548e9e4e59ddc3925f078b4590364
Ellie <elinorj@byu.edu>
2025-06-09 11:09:06 -0600

    only allowing using ftl or sublight at a time, not both.

--- dev-fd/index.js
+++ dev-fd/index.js
@@ -1,20 +1,28 @@
-console.log("index.js loaded");
-var systems = {
-
-}
+var systems = {}
 
 var processResults = (results, cursor) => {
     var systemsUpdated = new Set();
     results.forEach(result => {
-        //todo: we only want to add the systems updated if the result type is a system update => if result.type == 'state-updated'
-        systemsUpdated.add(result.system);
+        if (result.type === 'state-updated') {
+            systemsUpdated.add(result.system);
+        }
         systems[result.system] = {
             ...systems[result.system],
             ...result.payload
         };
     });
-    if (systemsUpdated.has('ftl-engines')) {
-        document.getElementById("speedValue").innerText = systems['ftl-engines'].currentSpeed || 'no speed found';
+    if (systemsUpdated.has('ftl-engines') || systemsUpdated.has('sublight-engines')) {
+        const sublightSpeed = systems['sublight-engines'] ? systems['sublight-engines'].currentSpeed : 0;
+        const ftlSpeed = systems['ftl-engines'] ? systems['ftl-engines'].currentSpeed : 0;
+        if (ftlSpeed == 0 && sublightSpeed == 0) {
+            document.getElementById("speedValue").innerText = "Full Stop";
+        }
+        else if (ftlSpeed > 0 && sublightSpeed == 0) {
+            document.getElementById("speedValue").innerText = `FTL - ${ftlSpeed}`;
+        }
+        else {
+            document.getElementById("speedValue").innerText = `Sublight - ${sublightSpeed}`;
+        }
     }
 };
 
@@ -31,9 +39,75 @@ var init = async () => {
 
     document.getElementById("changeSpeed").addEventListener("click", () => {
         console.log(api);
-        api.sendCommand("set-ftl-engines-speed", {
-            speed: 3
-        });
+        const givenSpeed = document.getElementById("speedOptions").value;
+        switch (givenSpeed) {
+            case 'fullStop':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-1':
+                api.sendCommand("set-ftl-engines-speed", { speed: 1 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-2':
+                api.sendCommand("set-ftl-engines-speed", { speed: 2 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-3':
+                api.sendCommand("set-ftl-engines-speed", { speed: 3 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-4':
+                api.sendCommand("set-ftl-engines-speed", { speed: 4 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-5':
+                api.sendCommand("set-ftl-engines-speed", { speed: 5 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-6':
+                api.sendCommand("set-ftl-engines-speed", { speed: 6 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-7':
+                api.sendCommand("set-ftl-engines-speed", { speed: 7 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-8':
+                api.sendCommand("set-ftl-engines-speed", { speed: 8 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-9':
+                api.sendCommand("set-ftl-engines-speed", { speed: 9 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'ftl-10':
+                api.sendCommand("set-ftl-engines-speed", { speed: 10 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 0 });
+                break;
+            case 'sublight-1':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 1 });
+                break;
+            case 'sublight-2':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 2 });
+                break;
+            case 'sublight-3':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 3 });
+                break;
+            case 'sublight-4':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 4 });
+                break;
+            case 'sublight-5':
+                api.sendCommand("set-ftl-engines-speed", { speed: 0 });
+                api.sendCommand("set-sublight-engines-speed", { speed: 5 });
+                break;
+            default:
+                console.log('Unknown state.');
+        }
     });
 }
 
